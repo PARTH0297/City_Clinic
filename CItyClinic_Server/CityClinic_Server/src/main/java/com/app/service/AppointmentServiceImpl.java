190e0d332ff5ef82f3a8f99349d6db647ff6719d
PARTH KADAM <parthkadam297@gmail.com>
2024-08-15 20:44:54 +0530

    v2

--- CItyClinic_Server/CityClinic_Server/src/main/java/com/app/service/AppointmentServiceImpl.java
+++ CItyClinic_Server/CityClinic_Server/src/main/java/com/app/service/AppointmentServiceImpl.java
@@ -11,7 +11,13 @@ import org.springframework.stereotype.Service;
 
 import com.app.dto.AppointmentDTO;
 import com.app.entity.Appointment;
+import com.app.entity.Doctor;
+import com.app.entity.Patient;
+import com.app.entity.User;
 import com.app.repository.IAppointmentRepository;
+import com.app.repository.IDoctorRepository;
+import com.app.repository.IPatientRepository;
+import com.app.repository.IUserRepository;
 
 @Service
 @Transactional
@@ -23,9 +29,31 @@ public class AppointmentServiceImpl implements AppointmentService {
 	@Autowired
 	private ModelMapper mapper;
 
+	@Autowired
+	private IPatientRepository patientRepo;
+
+	@Autowired
+	private IUserRepository userrepo;
+
+	@Autowired
+	private IDoctorRepository doctorRepo;
+
 	@Override
 	public AppointmentDTO createAppointment(AppointmentDTO appointmentDto) {
+
+		@SuppressWarnings("deprecation")
+		User user = userrepo.getById(appointmentDto.getPatientId());
+		Patient patient = patientRepo.findByUser(user);
+		appointmentDto.setPatientId(patient.getId());
+
+		Doctor doctor = doctorRepo.findById(appointmentDto.getDoctorId())
+				.orElseThrow(() -> new RuntimeException("Doctor not found"));
+
+		System.out.println("________" + appointmentDto);
+
 		Appointment appointment = mapper.map(appointmentDto, Appointment.class);
+		appointment.setDoctor(doctor); // Set the doctor
+		appointment.setPatient(patient); // Set the patient
 		appointment = appointmentRepo.save(appointment);
 
 		return mapper.map(appointment, AppointmentDTO.class);
@@ -34,8 +62,8 @@ public class AppointmentServiceImpl implements AppointmentService {
 	@Override
 	public AppointmentDTO updateAppointment(Long appointmentId, AppointmentDTO appointmentDto) {
 		Appointment appointment = appointmentRepo.findById(appointmentId)
-				.orElseThrow(() -> new RuntimeException("Appointment not found with id : "+appointmentId));
-		
+				.orElseThrow(() -> new RuntimeException("Appointment not found with id : " + appointmentId));
+
 //		appointment.setPatient(appointmentDto.getPatientId());
 		return null;
 	}
@@ -43,27 +71,25 @@ public class AppointmentServiceImpl implements AppointmentService {
 	@Override
 	public AppointmentDTO getAppointmentById(Long appointmentId) {
 		Appointment appointment = appointmentRepo.findById(appointmentId)
-				.orElseThrow(() -> new RuntimeException("Appointment not found with id : "+appointmentId));
-		
+				.orElseThrow(() -> new RuntimeException("Appointment not found with id : " + appointmentId));
+
 		return mapper.map(appointment, AppointmentDTO.class);
 	}
 
 	@Override
 	public List<AppointmentDTO> getAllAppointments() {
 		List<Appointment> appointments = appointmentRepo.findAll();
-		
-		return appointments
-                .stream()
-                .map(appointment -> mapper.map(appointment, AppointmentDTO.class))
-                .collect(Collectors.toList());
+
+		return appointments.stream().map(appointment -> mapper.map(appointment, AppointmentDTO.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public void deleteAppointment(Long appointmentId) {
 		Appointment appointment = appointmentRepo.findById(appointmentId)
-				.orElseThrow(() -> new RuntimeException("Appointment not found with id : "+appointmentId));
+				.orElseThrow(() -> new RuntimeException("Appointment not found with id : " + appointmentId));
 		appointmentRepo.delete(appointment);
-		
+
 	}
 
 }
--- city-clinic-frontend/src/Routers/router.jsx
+++ city-clinic-frontend/src/Routers/router.jsx
@@ -3,18 +3,21 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import App from '../App';
-import DashboardLayout from "../dashboard/DashboardLayout";
 import Dashboard from "../dashboard/Dashboard";
-import AppointmentManagement from "../dashboard/AppointmentManagement"; 
+
 import MedicalHistory from "../dashboard/MedicalHistory"; 
 import ReviewManagement from "../dashboard/ReviewManagement"; 
 import UserManagement from "../dashboard/UserManagement"; 
 import UserDetail from "../dashboard/UserDetail"; 
+import Clinic from "../dashboard/Clinic";
+import DoctorListForAppointments from "../dashboard/DoctorListForAppointments";
+import BookAppointment from "../dashboard/BookAppointment";
 import Login from "../Components/Login";
 import HomeComp from "../Components/HomeComp"; // Updated to HomeComp based on the first example
 import NotFound from "../Components/NotFound"; // Import the NotFound component
 import Register from '../Components/Register';
 import Profile from '../Components/Profile';
+import ListOfClinics from '../Components/ListOfClinics';
 
 const router = createBrowserRouter([
     {
@@ -30,49 +33,55 @@ const router = createBrowserRouter([
                 element: <Register />,
             },
             {
-                path: "/dashboard",
-                element: <DashboardLayout />,
-                children: [
-                    {
-                        path: "", // Matches /admin/dashboard
-                        element: <Dashboard />
-                    },
-                    {
-                        path: "appointments",
-                        element: <AppointmentManagement />
-                    },
-                    {
-                        path: "medical-history",
-                        element: <MedicalHistory />
-                    },
-                    {
-                        path: "reviews",
-                        element: <ReviewManagement />
-                    },
-                    {
-                        path: "users",
-                        element: <UserManagement />
-                    },
-                    {
-                        path: "user/:id",
-                        element: <UserDetail />,
-                        loader: ({ params }) => fetch(`http://localhost:8080/users/${params.id}`)
-                    }
-                ]
+                path: "/dashboard", // Directly render the Dashboard component
+                element: <Dashboard />, // Removed DashboardLayout
+            },
+            {
+                path: "/dashboard/medical-history",
+                element: <MedicalHistory />,
+            },
+            {
+                path: "/dashboard/reviews",
+                element: <ReviewManagement />,
+            },
+            {
+                path: "/dashboard/users",
+                element: <UserManagement />,
+            },
+            {
+                path: "/dashboard/clinic",
+                element: <Clinic />, // Route for the Clinic component
+            },
+            {
+                path: "/book-appointment",
+                element: <BookAppointment />, // Route for the BookAppointment component
+            },
+            {
+                path: "/dashboard/doctors", // New route for viewing doctors
+                element: <DoctorListForAppointments />, // Route for the new component
+            },
+            {
+                path: "/dashboard/user/:id",
+                element: <UserDetail />,
+                loader: ({ params }) => fetch(`http://localhost:8080/users/${params.id}`)
+            },
+            {
+                path: "/ListOfClinics",
+                element: <ListOfClinics />,
+            },
+            {
+                path: "/profile",
+                element: <Profile />,
+            },    
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "*", // This catches all unmatched routes
+                element: <NotFound /> // Render the NotFound component for unmatched routes
             }
         ]
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-    },    
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "*", // This catches all unmatched routes
-        element: <NotFound /> // Render the NotFound component for unmatched routes
     }
 ]);
 
